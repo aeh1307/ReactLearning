b08886b22ab4b85d372864dda4b81f6a960b469d
Astrid E Hamre <astridehamre@outlook.com>
2019-06-13 15:30:52 +0200

    fix up from last project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,131 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Person from './Person/Person'; 
 
+
+class App extends Component{
+state = {
+      persons: [
+        {name: 'Max', age: 28},
+        {name: 'Manu', age: 29},
+        {name: 'Stephanie', age: 26}
+        ],
+        otherState: 'Some other value',
+        showPersons: false
+    };
+
+switchNameHandler = (newName) => {
+      // console.log('Was clicked!');
+       // Don't do this:  this.state.persons[0].name = 'Maximilian'; 
+      this.setState( {
+       persons: [
+       {name: newName, age: 28},
+       {name: 'Manu', age: 29},
+       {name: 'Stephanie', age: 26} 
+     ]
+    } )
+   } 
+
+   nameChangedHandler = (event, id) => {
+
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id == id; 
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }; 
+
+   // alternative:  const person = Object.assign({}, this.state.persons[personIndex]);
+
+   person.name = event.target.value; 
+
+   const persons = [...this.state.persons];
+   persons[personIndex] = person; 
+
+    this.setState( {
+      persons: [
+      {id: 'person1',name: 'Max', age: 28},
+      {id: 'person2',name: event.target.value, age: 29},
+      {id: 'person3',name: 'Stephanie', age: 26}
+    ]
+   } )
+
+   }
+
+
+   togglePersonHandler = () => {
+    const doesShow = this.state.showPersons; 
+    this.setState({showPersons: !doesShow});
+   }
+
+   deletePersonHandler = (personIndex) => {
+     // const persons = this.state.persons.slice(); below more modern:
+      const persons = [...this.state.persons];
+      persons.splice(personIndex, 1);
+      this.setState({persons: persons});
+   }
+  
+    render(){
+
+
+      const style = {
+        backgroundColor: 'green',
+        color: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+      };
+      
+      let persons = null; 
+
+
+      if(this.state.showPersons){
+          persons = (
+            <div >
+              {this.state.persons.map((person, index) => {
+                  return <Person 
+                  click={() => this.deletePersonHandler(index)}
+                  name={person.name} 
+                  age={person.age}
+                  key={person.id}
+                  changed={(event) => this.nameChangedHandler(event, person.id)}/>
+              })}
+            </div> 
+          );
+
+          style.backgroundColor = 'red';   
+        }
+
+      const classes = [];  
+      if(this.state.persons.length <=2){
+        classes.push('red');  // classes = ['red']
+      }
+      if(this.state.persons.length <= 1){
+        classes.push('bold'); // classes = ['red', 'bold']
+      }
+
+    return (
+
+      <div className="App"> 
+        <h1>Hi, I'm a react App</h1>
+        <p className={classes.join(' ')}>This is really working</p>
+        <button 
+        style = {style}
+        onClick={ () => this.togglePersonHandler('MAximillian')}>Toggle Persons</button>
+        {persons}
+      </div>
+  
+    );
+  // return React.createElement('div', {className: 'App' }, 'h1', 'Hi, I\'m a react App!!!');
+    };
+
+  }
 export default App;
+
+
+
+//Have to use className instead of class because class is already used
+// Uses jSX which isn't html bu in most cases works and, looks just like it. 

25e00fac4f250bac8027b92effa34ea41aed7f8d
Astrid E Hamre <astridehamre@outlook.com>
2019-06-15 11:27:02 +0200

    ..

--- src/components/cockpit/Cockpit.js
+++ src/components/cockpit/Cockpit.js
@@ -1,33 +1,64 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import classes from './Cockpit.css';
 
 
-const cockpit = (props) => {
-    const assignedClasses = [];
-    let btnClass = '';
-    if(props.showPersons){
-        btnClass = classes.Red;
-    }
+// A functional component => cannot use react lifecycle hooks. 
+// However we can use antoher hook fromr react called useEffect.
+//useEffect combiens the fuctionality or the usecase you cna cover of 
+//all these class based life cycle hooks in one react  hook. 
 
 
-    if (props.persons.length <= 2) {
-      assignedClasses.push(classes.Red);  // classes = ['red']
-    }
-    if (props.persons.length <= 1) {
-      assignedClasses.push(classes.bold); // classes = ['red', 'bold']
-    }
+//ComponentDidMount and ComponentDidUpdate combiend in one effect: 
+const cockpit = props => {
+  useEffect(() => {
+    console.log('[Cockpit.js] useEffect');
+    // Http request...faked:
+    setTimeout(() => {
+      alert('Saved data to cloud!');
+    }, 1000);
+    return () => {
+      console.log('[Cockpit.js] cleanup work in useEffect');
+    };
+  }, []);
 
-    return (
-        <div className={classes.Cockpit}>
-            <h1>Hi, I'm a react App</h1>
-            <p className={assignedClasses.join(' ')}>This is really working</p>
-            <button
-                className={btnClass}
-                onClick={props.clicked}>Toggle Persons</button>
+  useEffect(() => {
+    console.log('[Cockpit.js] 2nd useEffect');
+    return () => {
+      console.log('[Cockpit.js] cleanup work in 2nd useEffect');
+    };
+  });
 
-        </div>
-    );
+//If you just want ComponentDidMount, you can use useEffect with an empty array
+//as second argument.
+
+  //If you have different effects than depends on different data,
+  // you can have as many useEffect setups as you want. 
+  //useEffect(); 
+
+  const assignedClasses = [];
+  let btnClass = '';
+  if (props.showPersons) {
+    btnClass = classes.Red;
+  }
+
+
+  if (props.persons.length <= 2) {
+    assignedClasses.push(classes.Red);  // classes = ['red']
+  }
+  if (props.persons.length <= 1) {
+    assignedClasses.push(classes.bold); // classes = ['red', 'bold']
+  }
+
+  return (
+    <div className={classes.Cockpit}>
+      <h1>{props.title}</h1>
+      <p className={assignedClasses.join(' ')}>This is really working</p>
+      <button className={btnClass} onClick={props.clicked}>
+        Toggle Persons
+        </button>
+    </div>
+  );
 
 };
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -5,6 +5,16 @@ import Cockpit from '../components/cockpit/Cockpit';
 
 
 class App extends Component {
+
+
+constructor(props){
+  super(props); 
+  console.log('[App.js] constructor');
+  //Can initialize the state in the cosntructor: with this.state =..
+}
+
+//More modern syntax to write state... autmoatically complies it to get 
+//initialized in the constructor....
   state = {
     persons: [
       { id: 'person1', name: 'Max', age: 28 },
@@ -12,20 +22,46 @@ class App extends Component {
       { id: 'person3', name: 'Stephanie', age: 26 }
     ],
     otherState: 'Some other value',
-    showPersons: false
+    showPersons: false,
+    showCockpit: true
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // Don't do this:  this.state.persons[0].name = 'Maximilian'; 
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  static getDerivedStateFromProps(props, state){
+    console.log('[App.js] getDerivedStateFromProps', props);
+    return state;
   }
+
+
+  //Only old versions of react will support this=> 
+  // componentWillMount(){
+  //   console.log('[App.js] componentWillMount'); 
+  // }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount'); 
+  }
+
+  shouldComponentUpdate(nexProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
+
+
+
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // Don't do this:  this.state.persons[0].name = 'Maximilian'; 
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 26 }
+  //     ]
+  //   })
+  // }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -43,53 +79,55 @@ class App extends Component {
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
-    this.setState({
-      persons: [
-        { id: 'person1', name: 'Max', age: 28 },
-        { id: 'person2', name: event.target.value, age: 29 },
-        { id: 'person3', name: 'Stephanie', age: 26 }
-      ]
-    })
+    this.setState({persons: persons});
+  };
 
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice(); below more modern:
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons});
   }
-
 
   togglePersonHandler = () => {
     const doesShow = this.state.showPersons;
     this.setState({ showPersons: !doesShow });
   }
 
-  deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons.slice(); below more modern:
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({ persons: persons });
-  }
-
   render() {
-
+    console.log('[App.js] render');
     let persons = null;
 
     if (this.state.showPersons) {
-      persons = <Persons
+      persons = (
+          <Persons
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
-          changed={this.nameChangedHandler} />;
+          changed={this.nameChangedHandler} 
+          />
+      );
     }
 
     return (
-
       <div className={classes.App}>
-        <Cockpit
+        <button onClick={() => {
+          this.setState({showCockpit: false}); 
+        }}>
+          Remove Cockpit
+          </button>
+          {this.state.showCockpit ? (
+          <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
-          clicked={this.togglePersonHandler} />
+          clicked={this.togglePersonHandler} 
+          /> 
+          ) : null}
         {persons}
       </div>
-
     );
     // return React.createElement('div', {className: 'App' }, 'h1', 'Hi, I\'m a react App!!!');
-  };
+  }
 
 }
 export default App;
